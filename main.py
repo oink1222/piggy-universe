c61caa60a9dfde029d8641022aa5fef4af0657f8
oink1222 <94766428+oink1222@users.noreply.github.com>
2022-03-12 22:49:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from time import sleep
+print("loading...")
+sleep(2)
+print("powered by pygame")
+sleep(2)
+print("by oink studios")
+sleep(5)
+print("version 0.1")
+sleep(5)
+import pygame
+
+pygame.init()
+
+pygame.display.init()
+
+pygame.display.set_mode()
+
+while True:
+  pygame.display.flip()
